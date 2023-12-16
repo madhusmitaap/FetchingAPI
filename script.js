0a9389f7010efbbeaf2dd371b7b96eab35be71c9
Madhusmita Pradhan <pmadhusmita63@gmail.com>
2023-12-17 01:34:24 +0530

    commits

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    data.forEach((user) => {
+      const markup = `<li>${user.name}</li>`;
+      document.querySelector("ul").insertAdjacentHTML("beforeend", markup);
+    });
+  })
+  .catch((error) => console.log(error));
